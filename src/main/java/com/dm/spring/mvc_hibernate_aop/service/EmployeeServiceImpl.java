4f6a69790b555d7acd92d6bdaaf535a22335e171
John Doe <methodvr3@gmail.com>
2022-09-27 23:24:00 +0300

    Study of spring (Spring_MVC_Hibernate) day 12

---
+++ src/main/java/com/dm/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.dm.spring.mvc_hibernate_aop.service;
+
+import com.dm.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.dm.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+       return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+}
